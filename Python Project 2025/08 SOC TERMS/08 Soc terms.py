9e22a402b52ec29f3be5db65a1373f72c6a85c6a
Alaa Mostafa <alaamos2017@yahoo.com>
2025-11-09 07:12:53 +0200

    Add files via upload

---
+++ Python Project 2025/08 SOC TERMS/08 Soc terms.py
@@ -0,0 +1,21 @@
+        #Task 8 :
+ # ?? # #??# #?? The 6 Most Important Sociology Terms :-
+print("Welcome to the Social terms section")
+
+term=input("Please Enter the Social Term: ")
+
+if term == "Society":
+  print("""A group of people living together within an organized framework governed by rules and laws.""")
+elif term == "Culture":
+    print("The set of values, customs, traditions, knowledge, and symbols that distinguish a society.")
+elif term == "Social Structure":
+    print("The organized framework of relationships and roles among individuals and groups within society.")
+elif term == "Social Institutions":
+    print('Fundamental systems that organize people"s lives, such as family, education, religion, economy, and politics.')
+elif term == "Values and Norms":
+    print("Values are what society considers important and good,while norms are the behavioral rules people are expected to follow.")
+elif term == "Socialization":
+    print('The process through which individuals learn society’s values and norms from childhood to become integrated members.')
+
+else:
+    print("No answer ")
